36e8b565ac2043743037479f7c58c1d3b127e86d
geniusrabbit <U201613566@hust.edu.cn>
2019-03-03 06:08:07 +0800

    done software

--- Software/function.h
+++ Software/function.h
@@ -4,17 +4,40 @@
 #define	XC_HEADER_TEMPLATE_H
 
 #include <xc.h> // include processor files - each processor file is guarded.  
+#define TRUE 1
+#define FALSE 0
+#define RECV_BUF_MAX 20
 
 /**
  * public data here
  */
 // time data
-int g_time_hour;
-int g_time_minute;
-int g_time_second;
-int g_time_unit;
+int g_time_h;
+int g_time_m;
+int g_time_s;
+int g_time_u;
 
+// bool value used in receive_decode
+int g_start_read_switch; // switch
+int g_start_read_data;   // start read data
+int g_find_recv_start;
 
+// times value in receive_decode
+int g_high_level_times;
+int g_all_level_times;
+int g_recv_count;
+// read time buf in receive_code
+int g_recv_buf[RECV_BUF_MAX];
+
+/**
+ * test value
+ */
+#define TEST
+#ifdef TEST
+int g_all_time_count;
+int test_buf_index;
+int test_buf[20];
+#endif
 // all functions
 /**
  * @func:receive signal and decode
